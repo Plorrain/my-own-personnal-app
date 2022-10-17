c222f5857f3d19cd5b5c217f9c8308253524050e
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-17 15:43:46 -0400

    the in progress message centered

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className='container'>
-      <h3>Hi I'm Philippine! This app is progress👷‍♀️. More to comme soon!</h3>
+      <h3 align="center">Hi I'm Philippine! This app is progress👷‍♀️. More to comme soon!</h3>
     </div>
   );
 }
