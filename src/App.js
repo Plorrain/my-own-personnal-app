c405c5059cfc92e184a03608ba5e9f47c18edecb
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-24 16:25:55 -0400

    back to original code in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import ReactCardFlip from "../src/components/UI/ReactCardFlip";
+import React, { useState } from 'react';
+import ReactCardFlip from "react-card-flip";
 
-function App(props) {
-  // const [flip, setFlip] = useState(false);
+function App() {
+  const [flip, setFlip] = useState(false);
   return (
-      <ReactCardFlip />
+      <ReactCardFlip isFlipped={flip}
+          flipDirection="vertical">
+          <div style={{
+              width: '300px',
+              height: '200px',
+              background: '#d7fbda',
+              fontSize: '40px',
+              color: 'green',
+              margin: '20px',
+              borderRadius: '4px',
+              textAlign: 'center',
+              padding: '20px'
+          }}>
+              Welcome to GFG.
+              <br />
+              <br />
+              <button style={{
+                  width: '150px',
+                  padding: '10px',
+                  fontSize: '20px',
+                  background: '#f5d9fa',
+                  fontWeight: 'bold',
+                  borderRadius: '5px'
+              }} onClick={() => setFlip(!flip)}>
+                  Flip</button>
+          </div>
+          <div style={{
+              width: '300px',
+              height: '200px',
+              background: '#fbd7f8',
+              fontSize: '40px',
+              color: 'blue',
+              margin: '20px',
+              borderRadius: '4px',
+              textAlign: 'center',
+              padding: '20px'
+          }}>
+              Computer Science Portal.
+              <br />
+              <button style={{
+                  width: '150px',
+                  padding: '10px',
+                  fontSize: '20px',
+                  background: '#f5d9fa',
+                  fontWeight: 'bold',
+                  borderRadius: '5px'
+              }} onClick={() => setFlip(!flip)}>
+                  Flip</button>
+          </div>
+      </ReactCardFlip>
   );
 }
 export default App;
