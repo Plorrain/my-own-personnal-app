a0033e73dd47ca845cf8dd32fb22927963b02e9b
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-27 15:54:05 -0400

    trying to break card flip component:css + js file. Managed to move it from App.js

---
+++ src/components/CardFlip.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import ReactCardFlip from "react-card-flip";
+import './CardFlip.css';
+
+function CardFlip() {
+  const [flip, setFlip] = useState(false);
+  return (
+      <ReactCardFlip isFlipped={flip}
+          flipDirection="vertical">
+          <div className='front-card'>
+
+          {/* style={{
+              width: '300px',
+              height: '200px',
+              background: '#e0edff',
+              border:'2px solid #D8D8D8',
+              boxShadow: '2px 2px #D8D8D8',
+              fontFamily: 'Verdana, sans-serif',
+              fontSize: '30px',
+              color: '#1F1D36',
+              margin: '20px',
+              borderRadius: '4px',
+              textAlign: 'center',
+              padding: '20px'
+          }} */}
+              Hi✨ my name is Philippine...
+              <br />
+              <br />
+              <button style={{
+                  width: '150px',
+                  padding: '13px',
+                  fontSize: '15px',
+                  background: '#FEC260',
+                  fontWeight: 'bold',
+                  color: '#787A91',
+                  borderRadius: '5px'
+              }} onClick={() => setFlip(!flip)}>
+                  Learn more</button>
+          </div>
+          <div style={{
+              width: '300px',
+              height: '200px',
+              background: '#FEC260',
+              border:'2px solid #D8D8D8',
+              boxShadow: '2px 2px #D8D8D8',
+              fontSize: '30px',
+              fontFamily: 'Verdana, sans-serif',
+              color: '#787A91',
+              margin: '20px',
+              borderRadius: '4px',
+              textAlign: 'center',
+              padding: '20px'
+          }}>
+              I made this card using React.
+              <br />
+              <button style={{
+                  width: '150px',
+                  padding: '15px',
+                  marginTop: '40px',
+                  fontSize: '15px',
+                  background: '#e0edff',
+                  color: '#1F1D36',
+                  fontWeight: 'bold',
+                  borderRadius: '7px'
+              }} onClick={() => setFlip(!flip)}>
+                  Flip Back</button>
+          </div>
+      </ReactCardFlip>
+  );
+}
+
+export default CardFlip;
