c5b6ccaf6004ae3875a7bf370ae080b668c9fc5e
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-27 16:35:53 -0400

    css separation succesfful

--- src/components/CardFlip.js
+++ src/components/CardFlip.js
@@ -8,65 +8,21 @@ function CardFlip() {
       <ReactCardFlip isFlipped={flip}
           flipDirection="vertical">
         <div className='front-card'>
-          {/* style={{
-              width: '300px',
-              height: '200px',
-              background: '#e0edff',
-              border:'2px solid #D8D8D8',
-              boxShadow: '2px 2px #D8D8D8',
-              fontFamily: 'Verdana, sans-serif',
-              fontSize: '30px',
-              color: '#1F1D36',
-              margin: '20px',
-              borderRadius: '4px',
-              textAlign: 'center',
-              padding: '20px'
-          }} */}
-              Hi✨ my name is Philippine...
-              <br />
-              <br />
-              <button className='button-front-card'
+          Hi✨ my name is Philippine...
+            <br />
+            <br />
+            <button className='button-front-card'
               onClick={() => setFlip(!flip)}>
                 Learn more
-              </button>
-              {/* style={{
-                  width: '150px',
-                  padding: '13px',
-                  fontSize: '15px',
-                  background: '#FEC260',
-                  fontWeight: 'bold',
-                  color: '#787A91',
-                  borderRadius: '5px'  }}*/}
-          </div>
-          <div
-              // style={{
-              // width: '300px',
-              // height: '200px',
-              // background: '#FEC260',
-              // border:'2px solid #D8D8D8',
-              // boxShadow: '2px 2px #D8D8D8',
-              // fontSize: '30px',
-              // fontFamily: 'Verdana, sans-serif',
-              // color: '#787A91',
-              // margin: '20px',
-              // borderRadius: '4px',
-              // textAlign: 'center',
-              // padding: '20px'}}
-              >I made this card using React.
-              <br />
-              <button className='button-back-card'
-                  // style={{
-                  // width: '150px',
-                  // padding: '15px',
-                  // marginTop: '40px',
-                  // fontSize: '15px',
-                  // background: '#e0edff',
-                  // color: '#1F1D36',
-                  // fontWeight: 'bold',
-                  // borderRadius: '7px' }}
-               onClick={() => setFlip(!flip)}>
-                  Flip Back</button>
-          </div>
+            </button>
+        </div>
+        <div className='back-card'> I made this card using React.
+          <br />
+            <button className='button-back-card'
+              onClick={() => setFlip(!flip)}>
+              Flip Back
+            </button>
+        </div>
       </ReactCardFlip>
   );
 }
