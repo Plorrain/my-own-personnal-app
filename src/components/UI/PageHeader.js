bd3da29322b98884dea227274d43dd9a3c8ab004
Philippine Lorrain <philippinelorrain@gmail.com>
2022-11-02 14:40:02 -0400

    putting correct information at least while I try to figure out how to fix navbar

--- src/components/UI/PageHeader.js
+++ src/components/UI/PageHeader.js
@@ -20,7 +20,7 @@ function PageHeader() {
     <div className="lg:flex lg:items-center lg:justify-between">
       <div className="min-w-0 flex-1">
         <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-          Back End Developer
+          Fullstack Developer
         </h2>
         <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
           <div className="mt-2 flex items-center text-sm text-gray-500">
@@ -29,15 +29,15 @@ function PageHeader() {
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <MapPinIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-            Remote
+            Montreal, QC
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <CurrencyDollarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-            $120k &ndash; $140k
+            $50k &ndash; $55k
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <CalendarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-            Closing on January 9, 2020
+            Closing on December, 1 2022
           </div>
         </div>
       </div>
