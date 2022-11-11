28f142eb4329b73893d7d4314bd424d213abbf02
Philippine Lorrain <philippinelorrain@gmail.com>
2022-11-11 12:27:58 -0500

    keeping on css in index.css

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React, { Fragment } from 'react';
 
 import PageHeader from './components/UI/PageHeader';
 import CardFlip from './components/UI/CardFlip';
-import './App.css';
+
 
 function App() {
 return (
     <Fragment>
-      <div className='body'>
-        <h1>Philippine Lorrain</h1>
-        <PageHeader />
-        <CardFlip />
-      </div>
+      <h1>Philippine Lorrain</h1>
+      <PageHeader />
+      <CardFlip />
       <footer>
         Made with 💙 by Philippine Lorrain
       </footer>
