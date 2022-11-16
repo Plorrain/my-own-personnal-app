78afeca823e36cd9cf79e66bd758fe68d22aa5d8
Philippine Lorrain <philippinelorrain@gmail.com>
2022-11-16 17:04:01 -0500

    content for about me section

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ return (
       <img src={require('./images/profile-picture.jpeg').default} alt='Philippine Lorrain' />
       <PageHeader />
         <h1>Projects</h1>
+        <p>Brunchtime</p>
+        <h1>About me</h1>
+        <p>I used to work in retail/customer service. I decided to change careers
+          and ended up learning code in a bootcamp.
+          I am learning react at the moment
+          (and i used it for this website 😯)
+        </p>
       <CardFlip />
       <footer style={{backgroundColor: "#C1A3A3", color: "#F3C5C5"}}>
         Made with 🖤 by yours truly
