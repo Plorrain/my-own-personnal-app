e814e489d49b1ad7bc65975550b3903cd7046ee4
Philippine Lorrain <philippinelorrain@gmail.com>
2022-11-16 16:15:45 -0500

    css: trying to implement picture. Can't figure out the right path to write

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 
 import PageHeader from './components/UI/PageHeader';
 import CardFlip from './components/UI/CardFlip';
+// import image from '../public/images/';
 
 
 function App() {
@@ -10,6 +11,7 @@ return (
       <div className='name-banner'>
         <h1 className='my-name'>Philippine Lorrain</h1>
       </div>
+      <img src='../public/images/profile-picture.jpeg' alt='Philippine Lorrain' />
       <PageHeader />
       <CardFlip />
       <footer style={{backgroundColor: "#C1A3A3", color: "#F3C5C5"}}>
