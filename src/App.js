04defd2624a18b9267bacfb841e1b10007e80405
Philippine Lorrain <philippinelorrain@gmail.com>
2022-11-18 16:49:39 -0500

    css: trying to add padding to container class-->not working (probable overriding tw class or something)

--- src/App.js
+++ src/App.js
@@ -9,32 +9,34 @@ return (
 {/* lets make scrolling pretty */}
 
       {/* different colors for name banner */}
-      <div className='name-banner'>
-        <h1 className='my-name'>Philippine Lorrain</h1>
-      </div>
-      {/* image not working */}
-      <img src={require('./images/profile-picture.jpeg').default} alt='Philippine Lorrain' />
+        <div className='name-banner'>
+          <h1 className='my-name'>Philippine Lorrain</h1>
+        </div>
+      <div className='container'>
+        {/* image not working */}
+        <img src={require('./images/profile-picture.jpeg').default} alt='Philippine Lorrain' />
 
-      <PageHeader />
-        <h1>Projects</h1>
-      {/* brunchtime in a card component maybe? */}
-      <div>
-        <a href='https://www.brunchtime.live/' target='blank'>Brunchtime</a>
-        <ul>
-          <li>Teamwork project while at Le Wagon</li>
-        </ul>
-      </div>
+        <PageHeader />
+          <h1>Projects</h1>
+        {/* brunchtime in a card component maybe? */}
+        <div>
+          <a href='https://www.brunchtime.live/' target='blank'>Brunchtime</a>
+          <ul>
+            <li>Teamwork project while at Le Wagon</li>
+          </ul>
+        </div>
 
-        <h1>About me</h1>
-      <div>
-        <p>I used to work in retail/customer service. In 2022, decided to change careers
-          and ended up learning code in a bootcamp and I loved the experience.
-          I am currently learning react
-          (and i used it for this website 😯)!
-        </p>
+          <h1>About me</h1>
+        <div>
+          <p>I used to work in retail/customer service. In 2022, decided to change careers
+            and ended up learning code in a bootcamp and I loved the experience.
+            I am currently learning react
+            (and i used it for this website 😯)!
+          </p>
+        </div>
+        {/* make a list of skills (on flipcard?) */}
+        <CardFlip />
       </div>
-      {/* make a list of skills (on flipcard?) */}
-      <CardFlip />
       <footer style={{backgroundColor: "#C1A3A3", color: "#F3C5C5"}}>
         Made with 🖤 by yours truly
       </footer>
