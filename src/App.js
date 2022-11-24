6e18e51650a1036e494ac1a887076f079024deee
Philippine Lorrain <philippinelorrain@gmail.com>
2022-11-24 17:39:29 -0500

    css: centered image

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import PageHeader from './components/UI/PageHeader';
-import CardFlip from './components/UI/CardFlip';
-import profilePicture from './images/profile-picture.png'
+import PageHeader from "./components/UI/PageHeader";
+import CardFlip from "./components/UI/CardFlip";
+import profilePicture from "./images/profile-picture.png";
 
 function App() {
-return (
+  return (
     <Fragment>
-      <div className='container-full'>
-        <div className='name-banner'>
+      <div className="container-full">
+        <div className="name-banner">
           <h1>Philippine Lorrain</h1>
         </div>
       </div>
-      <div className='container'>
-        <img src={profilePicture} alt='Philippine Lorrain' />
+      <div className="container">
+        <img src={profilePicture} alt="Philippine Lorrain" />
         <PageHeader />
-          <h1>Projects</h1>
+        <h1>Projects</h1>
         {/* brunchtime in a card component maybe? */}
         <div>
-          <a href='https://www.brunchtime.live/' target='blank'>Brunchtime</a>
+          <a href="https://www.brunchtime.live/" target="blank">
+            Brunchtime
+          </a>
         </div>
         <div>
           <h2>Teamwork project while at Le Wagon</h2>
         </div>
         <h1>About me</h1>
         <div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat beatae, ab nostrum et excepturi aliquid
-            reprehenderit architecto rerum voluptas culpa consequuntur.
-            Earum voluptates non perferendis consequatur praesentium optio enim minima.
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+            beatae, ab nostrum et excepturi aliquid reprehenderit architecto
+            rerum voluptas culpa consequuntur. Earum voluptates non perferendis
+            consequatur praesentium optio enim minima.
           </p>
         </div>
-          {/* make a list of skills (on flipcard?) */}
+        {/* make a list of skills (on flipcard?) */}
         <CardFlip />
         <CardFlip />
       </div>
-      <div className='container-full'>
-        <footer>
-          Made with 🖤 by yours truly
-        </footer>
+      <div className="container-full">
+        <footer>Made with 🖤 by yours truly</footer>
       </div>
     </Fragment>
   );
