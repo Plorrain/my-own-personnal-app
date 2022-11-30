ee85e55c42b7846ef2ed99892f2f136d0b5cbded
Philippine Lorrain <philippinelorrain@gmail.com>
2022-11-29 19:04:50 -0500

    asset: added content to file: both english and french translated + will need to update with every additional text

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,1 +1,29 @@
-// francais and english languages
+const traductions = {
+  EN: {
+    projects: {
+      title: "Project(s)",
+      description: "Teamwork project while at Le Wagon",
+    },
+    aboutMe: {
+      title: "About Me",
+      txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat beatae, ab nostrum et excepturi aliquid reprehenderit architecto rerum voluptas culpa consequuntur. Earum voluptates non perferendis consequatur praesentium optio enim minima.",
+    },
+
+    footer: "Made with 🖤 by yours truly",
+  },
+
+  FR: {
+    projects: {
+      title: "Projet(s)",
+      description: "Teamwork project while at Le Wagon",
+    },
+    aboutMe: {
+      title: "Qui suis-je?",
+      txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat beatae, ab nostrum et excepturi aliquid reprehenderit architecto rerum voluptas culpa consequuntur. Earum voluptates non perferendis consequatur praesentium optio enim minima.",
+    },
+
+    footer: "Fait avec 🖤 par votre humble développeuse",
+  },
+};
+
+export default traductions;
