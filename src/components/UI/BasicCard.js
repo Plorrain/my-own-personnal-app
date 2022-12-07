78c5fdfa8616d7ef97d4572ed7fc77dbe14a0b2c
Philippine Lorrain <philippinelorrain@gmail.com>
2022-12-07 16:37:44 -0500

    card component: it's a disaster, I suspect its also not really for react:

--- src/components/UI/BasicCard.js
+++ src/components/UI/BasicCard.js
@@ -34,23 +34,17 @@ export default function BasicCard() {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
+        {/* <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        </Typography> */}
+        {/* <Typography sx={{ mb: 1.5 }} color="text.secondary">
         </Typography>
         <Typography variant="body2">
-          well meaning and kindly.
           <br />
           {'"a benevolent smile"'}
-        </Typography>
+        </Typography> */}
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button size="small">Bruncthime</Button>
       </CardActions>
     </Card>
   );
