db58df91f2e8d5b0fc816610d3ab5a6ac8f07663
Philippine Lorrain <philippinelorrain@gmail.com>
2022-12-21 16:57:28 -0500

    NavBar: installed code for logo but image does not show

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -21,13 +21,9 @@ export default function Example() {
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Logo */}
 
-                {/* <div className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    <img
-                            className="h-8 w-8 rounded-full"
-                            src="../images/"
-                            alt="Philippine Lorrain logo"
-                          />
-                    </div> */}
+                <div className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <img className="h-8 w-8 rounded-full" src="./images/logo.png" alt="Philippine Lorrain logo" />
+                </div>
 
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
