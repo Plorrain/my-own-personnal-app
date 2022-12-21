ebf13fd2150cc35d7d182d832cd585c4a6f900f4
Philippine Lorrain <philippinelorrain@gmail.com>
2022-12-21 15:59:38 -0500

    NavBar: found the line with the mobile menu text color

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -15,7 +15,7 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-white bg-opacity-50">
+    <Disclosure as="nav" className="bg-white bg-opacity-20">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -72,7 +72,7 @@ export default function Example() {
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-gray-900 text-white' : 'text-blue-900 hover:bg-gray-700 hover:text-white',
                     'block px-3 py-2 rounded-md text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
